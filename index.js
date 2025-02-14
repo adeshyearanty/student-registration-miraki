2fd8af8d380ddccea83a9c23ead89fce6cd9c74c
Adesh Yearanty <adesh.yearanty@gmail.com>
2025-02-14 19:06:31 +0530

    final commit

--- index.js
+++ index.js
@@ -3,127 +3,139 @@ import cors from 'cors'
 import mongoose from 'mongoose';
 import Student from './Student.js';
 import crypto from 'crypto';
+import dotenv from 'dotenv';
 
+dotenv.config()
 const app = express()
 
-const PORT = 3000;
-const url = `mongodb+srv://root:root@book-store-mern.gkvi8ub.mongodb.net/miraki?retryWrites=true&w=majority&appName=my-connection`
+const PORT = process.env.PORT || 3000
+const url = process.env.mongoDBUrl
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Hello"
-    })
-})
+function validate(res, fields) {
+    const patterns = {
+        email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        mobile: /^[6-9]\d{9}$/
+    };
+    for (const [field, value] of Object.entries(fields)) {
+        if (!patterns[field].test(value)) {
+            return returnResponse(res, `Please enter a valid ${field}..!!`, 400);
+        }
+    }
+    return true;
+}
+
+function returnResponse(res, msg, statusCode) {
+    return res.status(statusCode).json({ message: msg });
+}
 
 app.post('/register', async (req, res) => {
     try {
-        const { email, mobile } = req.body
-        const emailExisting = await Student.findOne({ email })
-        const mobileExisting = await Student.findOne({ mobile })
-        if (emailExisting) {
-            return res.status(400).json({
-                message: "Email already exists..!!" 
-            })
-        } else if (mobileExisting) {
-            return res.status(400).json({
-                message: "Mobile already exists..!!"
-            })
+        const { email, mobile, ...userData } = req.body;
+
+        if (!validate(res, { email, mobile })) return;
+        const existingUser = await Student.findOne({ $or: [{ email }, { mobile }] });
+
+        if (existingUser) {
+            let msg
+            if (existingUser.email === email) msg = "Email"
+            if (existingUser.mobile === mobile) msg = "Mobile"
+            return returnResponse(res, `${msg} already exists..!!`, 400);
         }
-        const user = req.body
-        if (req.body.mobile.toString().length == 10) {
-            user.address = []
-            await Student.create(user)
-            return res.status(200).json({
-                message: "User Successfully Inserted..!!" 
-            })
-        }
-        res.status(400).json({
-            message: "The length of Mobile Number should be 10..!!"
-        })
-    } catch (error) { 
-        console.error(error)
+        await Student.create({ email, mobile, ...userData, address: [] });
+        return returnResponse(res, "User Successfully Inserted..!!", 201);
+    } catch (error) {
+        console.error("Error in /register:", error);
+        return returnResponse(res, "Something Went Wrong..!!", 500);
     }
-})
+});
 
 app.post('/login', async (req, res) => {
     try {
-        const { email, mobile, password } = req.body
-        let user
-        if (email) {
-            user = await Student.findOne({ email, password })
-        } else if (mobile) {
-            user = await Student.findOne({ mobile, password })
-        } else {
-            res.status(400).json({
-                message: "email or mobile is required for login..!!"
-            })
+        const { email, mobile, password } = req.body;
+
+        if (!password || (!email && !mobile)) {
+            return returnResponse(res, "Email or Mobile and Password are required..!!", 400);
         }
-        if (user.token) {
-            return res.status(200).json({
-                email: user.email,
-                token: user.token
-            })
+
+        const user = await Student.findOne({ 
+            $or: [{ email }, { mobile }], 
+            password 
+        });
+
+        if (!user) return returnResponse(res, "Invalid email/mobile or password..!!", 401);
+
+        if (!user.token) {
+            user.token = crypto.randomBytes(16).toString('hex');
+            await user.save();
         }
-        const token = crypto.randomBytes(16).toString('hex')
-        user.token = token
-        await user.save()
-        res.status(200).json({
+
+        return res.status(200).json({
             email: user.email,
+            mobile: user.mobile,
             token: user.token
-        })
+        });
+
     } catch (error) {
-        console.error(error)
+        console.error("Error in /login:", error);
+        return returnResponse(res, "Internal Server Error..!!", 500);
     }
-})
+});
+
 
 app.post('/address', async (req, res) => {
     try {
-        const { email, mobile, password, address } = req.body
-        let user
-        if (email) {
-            user = await Student.findOne({ email, password })
-        } else if (mobile) {
-            user = await Student.findOne({ mobile, password })
-        } else {
-            return res.status(400).json({
-                message: "email or mobile is required..!!"
-            })
+        const { email, mobile, password, address } = req.body;
+
+        if (!email && !mobile) {
+            return returnResponse(res, "Email or Mobile is required..!!", 400);
         }
+        if (!password) {
+            return returnResponse(res, "Password is required..!!", 400);
+        }
+        if (!address) {
+            return returnResponse(res, "Address cannot be empty..!!", 400);
+        }
+
+        const user = await Student.findOne({ 
+            $or: [{ email }, { mobile }], 
+            password 
+        });
+
         if (!user) {
-            return res.status(404).json({
-                message: "User does not exist..!!"
-            })
+            return returnResponse(res, "User does not exist, please register..!!", 400);
         }
-        user.address.push(address)
-        await user.save()
-        res.status(201).json({
-            message: "Address Successfully Added..!!"
-        })
+
+        user.address.push(address);
+        await user.save();
+
+        return returnResponse(res, "User Address Added Successfully..!!", 200);
     } catch (error) {
-        console.error(error)
+        return returnResponse(res, "Something Went Wrong..!!", 500);
     }
-})
+});
+
 
 app.get('/me', async (req, res) => {
     try {
         const { token } = req.body
         const user = await Student.findOne({ token })
+        let msg, status = 200
         if (!user) {
-            return res.status(404).json({
-                message: `No user found for the token ${token}`
-            })
+            msg = `No user found for the token ${token}`
+            status = 404
         } 
-        res.status(201).json({
+        msg = {
             email: user.email,
             name: user.firstname + user.lastname,
             mobile: user.mobile,
             address: user.address
-        })
+        }
+        return returnResponse(res, msg, status)
     } catch (error) {
-        console.error(error)
+        return returnResponse(res, "Something Went Wrong..!!", 500)
     }
 })
 
